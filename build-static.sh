a4647a766ef5db9ddea1f3de4f0837b4caa1c029
Greg Johnson <gjohnson@gitlab.com>
2020-05-14 23:19:52 -0400

    update static content build script and regen bindata.go

--- build-static.sh
+++ build-static.sh
@@ -3,5 +3,5 @@
 #Script to generate code for ./core/bindata.go
 
 #install dependencies using the directions here:  https://github.com/elazarl/go-bindata-assetfs
-go-bindata-assetfs -o ./core/bindata.go -pkg "core" ./static/*
+go-bindata-assetfs -o ./core/bindata.go -pkg "core" ./static/dist/*
 go build
